b413ce283f073f0137be5c658be2240604d7bd65
AbhishekR14 <aranjan0288@gmail.com>
2024-04-08 15:26:30 +0530

    Step 13 w/o txc

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,43 @@
 const express = require("express");
+import { Account } from "../db";
+import { authMiddleware } from "../middlewares";
 
 const accountsRouter = express.router();
+
+accountsRouter.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({ userId: req.userId });
+  if (account) {
+    return res.status(200).send({
+      balance: account.balance,
+    });
+  }
+});
+
+accountsRouter.post("/transfer", authMiddleware, async (req, res) => {
+  const { amount, to } = req.body;
+  const account = await Account.findOne({ userId: req.userId });
+  if (account.balance < amount) {
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+  const toAccount = await Account.findOne({ userId: to });
+
+  if (!toAccount) {
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  );
+  await Account.updateOne({ userId: to }, { $inc: { balance: amount } });
+
+  res.json({
+    message: "Transfer successful",
+  });
+});
 
 export default accountsRouter;
