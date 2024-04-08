6caae6a5b6bd585f855eee73053f7daf08a0596f
AbhishekR14 <aranjan0288@gmail.com>
2024-04-08 17:24:44 +0530

    Backend complete

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -127,7 +127,7 @@ userRouter.get("/bulk", async (req, res) => {
   });
 
   res.json({
-    user: users.map((user) => ({
+    users: users.map((user) => ({
       username: user.username,
       firstName: user.firstName,
       lastName: user.lastName,
