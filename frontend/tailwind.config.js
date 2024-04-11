c9253e29bdb2f5f8506552c6b8e29a477f7046a7
Abhishek Ranjan <aranjan0288@gmail.com>
2024-04-11 13:07:32 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{html,js,ts,jsx,tsx}}",
-    "./components/**/*.{html,js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
