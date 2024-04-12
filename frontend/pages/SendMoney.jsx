f0035214467a84a0fc235fd5532f0e04b6ae07cf
AbhishekR14 <aranjan0288@gmail.com>
2024-04-12 14:04:10 +0530

    /Send backend-frontend

--- frontend/pages/SendMoney.jsx
+++ frontend/pages/SendMoney.jsx
@@ -1,12 +1,21 @@
 import axios from "axios";
 import { useSearchParams } from "react-router-dom";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const SendMoney = () => {
+  const [amount, setAmount] = React.useState(0);
+  const [resMessage, setResMessage] = React.useState("");
+  const [transcationStatus, setTranscationStatus] = React.useState(false);
   const [seachParams] = useSearchParams();
   const id = seachParams.get("id");
   const firstname = seachParams.get("firstname");
   const lastname = seachParams.get("lastname");
+  const navigate = useNavigate();
+  var body = {
+    amount: amount,
+    to: id,
+  };
   return (
     <div class="flex justify-center h-dvh bg-gray-100">
       <div className="h-full flex flex-col justify-center">
@@ -41,10 +50,42 @@ export const SendMoney = () => {
                   class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
+                  onChange={(event) => {
+                    setAmount(parseFloat(event.target.value));
+                  }}
                 />
               </div>
-              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-gray-800 text-white">
+              {resMessage != "" && <>{resMessage}</>}
+              <button
+                class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-gray-800 text-white"
+                onClick={async () => {
+                  const res = await axios.post(
+                    "https://payyourfren.onrender.com/api/v1/account/transfer",
+                    body,
+                    {
+                      headers: {
+                        authorization:
+                          "Bearer " + localStorage.getItem("authToken"),
+                      },
+                    }
+                  );
+                  if (res.status == 200) {
+                    setTranscationStatus(true);
+                    setResMessage(res.data.message);
+                  } else if (res.status == 400) {
+                    setResMessage(res.data.message);
+                  }
+                }}
+              >
                 Initiate Transfer
+              </button>
+              <button
+                class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-gray-800 text-white"
+                onClick={() => {
+                  navigate("/dashboard");
+                }}
+              >
+                Go Back
               </button>
             </div>
           </div>
