6228a85d9c5aea08e513dd0ea70665d4486f68ea
AbhishekR14 <aranjan0288@gmail.com>
2024-04-13 22:16:18 +0530

    removed double import

--- frontend/pages/signin.jsx
+++ frontend/pages/signin.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Heading } from "../components/heading";
 import { Subheading } from "../components/Subheading";
